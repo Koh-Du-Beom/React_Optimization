533744c4e5c0d4f820f9ae622101398793701069
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-20 03:32:46 +0900

    usecallback hook

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from 'react';
+import { useState, memo, useCallback } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -33,13 +33,13 @@ function Counter({ initialCount }) {
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(() => {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []) 
 
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+  const handleIncrement = useCallback(() => {
+		setCounter((prevCounter) => prevCounter + 1);
+	}, []) 
 
   return (
     <section className="counter">
