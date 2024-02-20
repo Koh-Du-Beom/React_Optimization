0a0575467156f7a329e488b9a44261c7c2367ad3
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-20 18:39:46 +0900

    key prop study

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,10 +1,11 @@
-import { useState, memo, useCallback } from 'react';
+import { useState, memo, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
   log(
@@ -29,16 +30,21 @@ function isPrime(number) {
 
 function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+	const [counterChanges, setCounterChanges] = useState([{value : initialCount, id : Math.random() * 1000}]);
+
+  const currentCounter = counterChanges.reduce(
+		(prevCounter, counterChange) => prevCounter + counterChange.value,
+		0
+	);
 
   const handleDecrement = useCallback(() => {
-    setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{value : -1, id : Math.random() * 1000 }, ...prevCounterChanges]);
   }, []) 
 
   const handleIncrement = useCallback(() => {
-		setCounter((prevCounter) => prevCounter + 1);
+		setCounterChanges((prevCounterChanges) => [{value : 1, id : Math.random() * 1000 }, ...prevCounterChanges]);
 	}, []) 
 
   return (
@@ -51,11 +57,12 @@ function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={counterChanges} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+			<CounterHistory history={counterChanges}/>
     </section>
   );
 };
